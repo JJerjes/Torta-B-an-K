3fad96d951654cc984891d4824b4dadcc17d476e
JJerjes <juniormariluz1@gmail.com>
2025-03-12 20:32:41 -0500

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,3 +5,26 @@ menuButton.addEventListener('click', () => {
     nav.classList.toggle('open');
     menuButton.classList.toggle('open');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const menuButton = document.querySelector('#menu');
+// const nav = document.querySelector('#nav');
+
+// menuButton.addEventListener('click', () => {
+//     nav.classList.toggle('open');
+//     menuButton.classList.toggle('open');
+// })
